4d3ce48e143aad11e73632a354e25ffe2d14c4f2
Ky Kyler <ky.kyler1@gmail.com>
2020-03-29 20:50:56 -0400

    updated JS code and style sheet

---
+++ .history/Assets/todo JS/todo_20200329203030.js
@@ -0,0 +1,37 @@
+// check off to do list items and change colors for different stages// in progress<--
+$("li").dblclick(function () {
+        if($(this).css("color") === "rgb(11, 209, 170)"){
+            $(this).css({
+                color: "gray",
+                textDecoration: "line-through"
+            });
+        }
+        else{
+            $(this).css({
+            color: "rgb(11, 209, 170)",
+            textDecoration: "none",
+            fontWeight: "bold"
+        });
+    }
+});
+
+$("li").click(function () {
+    // if Li is grey how do we change it back to black?
+    if($(this).css("color") === "rgb(128, 128, 128)"){
+        $(this).css({
+            color: "black",
+            textDecoration: "none"
+        });
+    }
+    else{
+        $(this).css({
+        color: "gray",
+        textDecoration: "line-through"
+    });
+}
+});
+
+// --->planner<--
+
+const currentTime = moment().format("H");
+    console.log(currentTime);
